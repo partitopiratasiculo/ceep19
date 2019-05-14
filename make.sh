3fa853c658aaca6307c0cdbe25e4941e0872e2f6
Giuseppe C <peppecal+gitid@gmail.com>
2019-05-14 16:32:12 +0200

    autogenerate all

--- make.sh
+++ make.sh
@@ -1,1 +1,7 @@
-pandoc --pdf-engine xelatex -o ceep19.pdf ceep19.text
+sizes=(10 12 14 16)
+for fontsize in "${sizes[@]}"; do
+	pandoc --pdf-engine xelatex --variable fontsize=${fontsize}pt -o ceep19-${fontsize}pt.pdf ceep19.text; done
+
+# 8pt uses twocolumns: 
+pandoc --pdf-engine xelatex --variable fontsize=8pt --variable classoption=twocolumn -o ceep19-8pt.pdf ceep19.text;
+
